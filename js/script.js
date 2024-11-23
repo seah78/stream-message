fbb7071af2c03d094d64c3a5298fbc696012cdde
SEAH <151732554+sebastienherlant@users.noreply.github.com>
2024-11-23 07:32:16 +0100

    init files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Sélectionne l'élément texte
+const animatedText = document.getElementById("animated-text");
+
+// Ajoute un gestionnaire de clic
+animatedText.addEventListener("click", () => {
+    // Redirige vers une URL dans un nouvel onglet
+    window.open("https://robertsspaceindustries.com/enlist?referral=STAR-FJG2-NYG4", "_blank");
+});
